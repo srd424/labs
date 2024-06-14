b3f9fe574e6ff545e4a6fe6ad5e90fc71baeece3
Jake Hamilton <jake.hamilton@hey.com>
2024-06-14 04:55:15 -0700

    refactor: enable foundation, standardize exports

--- tidepool/default.nix
+++ tidepool/default.nix
@@ -12,24 +12,13 @@
         {
           __file__ = ./default.nix;
 
-          # options.foundation = lib.options.create {
-          #   type = lib.types.attrs.of lib.types.derivation;
-          # };
+          options.foundation = lib.options.create {
+            type = lib.types.attrs.of lib.types.derivation;
+          };
 
-          # config.foundation = foundation;
-          # config.packages.foundation =
-          #   builtins.mapAttrs (name: package: {
-          #     name = package.name;
-
-          #     inherit package;
-
-          #     meta = package.meta;
-          #   })
-          #   foundation;
+          config.foundation = foundation;
         }
       ];
   };
 in
-  result.config
-# result.config.exported
-
+  result.config.exported
